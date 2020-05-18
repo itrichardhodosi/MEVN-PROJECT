d5b158a97c9bc05715af90c7733fe811056da8ab
Richard Hodosi <itrichardhodosi@gmail.com>
2020-05-17 19:44:57 -0700

    mayo 17 2020

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express=require('express');
-const app=express();
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+//const express=require('express');
+//const cors=require('cors')
+//const morgan=require('morgan');
+import path from 'path';
 
+const app=express();
 app.set('port',process.env.PORT || 3000);
 app.listen(app.get('port'),()=>{
     console.log('server on port  ' + app.get('port'));
+    console.log('ruta' + __dirname);
 });
+app.use(morgan('dev'));
+app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
